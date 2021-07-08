3c5fe9a1351fc3038be563ea435e753793182d53
olukade muzzammil <Adebayo0209@gmail.com>
2021-07-08 12:29:03 +0100

    feat: part1 exercise

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,18 @@
+const Part = (props)=>
+  <p>
+  {props.part} {props.exercise}
+</p>
+
+
+const Content = (props) => {
+  // console.log(props);
+  return(
+    <div>
+      <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises}/>
+      <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises}/>
+      <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises}/>
+    </div>
+  )
+}
+
+export default Content
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,2 @@
+const Total = (props) => <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+export default Total 
